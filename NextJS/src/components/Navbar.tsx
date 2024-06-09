25abecf3c99fe7e4cc870597b7b1ece177dad329
Thynne <khunthynne@gmail.com>
2024-06-10 03:51:19 +0700

    Nav bar demp

--- NextJS/src/components/Navbar.tsx
+++ NextJS/src/components/Navbar.tsx
@@ -1,6 +1,5 @@
-
-
 "use client"
+
 import { useEffect, useState } from "react";
 import CartLayOut from "./CouterBar/CartLayOut";
 import { getItem } from "@/lib/express_prisma";
@@ -10,7 +9,7 @@ export default function Navbar() {
     const [productsData, setProductsData] = useState<any>([]);
     const [error, setError] = useState<string | null>(null);
     const [load, setLoad] = useState<boolean>(false);
-
+    const [navOn, setNavOn] = useState<string>("");
     useEffect(() => {
         async function prismaGet() {
             try {
@@ -24,17 +23,47 @@ export default function Navbar() {
             }
 
         }
-
         prismaGet()
+
+        function handleScroll() {
+           
+            if (window.scrollY > lastScrollY) {
+               
+                setNavOn("down")
+            }
+            else if(window.scrollY === 0){
+
+                setNavOn("")
+            
+            } else {
+                
+                
+               setNavOn("up")
+            }
+            lastScrollY = window.scrollY;
+        }
+
+        let lastScrollY = window.scrollY;
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+
+
     }, [])
 
 
     return (
-        <div className="sticky h-full  top-0  bg-white shadow z-20 " style={{ position: "-webkit-sticky" }}>
+        <div className={`h-full  top-0   z-20  NavLayOut ${navOn}`}
+        // style={{ position: "-webkit-sticky" }}
+
+        >
+         
             <nav className=" relative flex justify-between items-center  container mx-auto   px-3  h-[80px]">
                 <div>Manu</div>
-             
-                <CartLayOut  Count={productsData.length} />
+
+                <CartLayOut Count={productsData.length} />
 
 
                 {/* <div className="absolute m-screen text-gray-300 z-[-99]"><h2 className="">Navbar layout.</h2>  </div> */}
